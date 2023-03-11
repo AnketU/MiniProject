99c40836cf1dcb36619e2ed8f361c54579e21a0d
AnketU <121781697+AnketU@users.noreply.github.com>
2023-03-11 16:58:17 +0530

    Add files via upload

---
+++ MiniProject/src/Hotel_Management_System/Not_Available.java
@@ -0,0 +1,18 @@
+package Hotel_Management_System;
+
+class NotAvailable extends Exception
+{
+    @Override
+    public String toString()
+    {
+        return "Not Available !";
+    }
+}
+
+class holder 
+{
+    Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
+    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
+    Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+}
---
+++ MiniProject/src/Hotel_Management_System/Singleroom.java
@@ -0,0 +1,45 @@
+package Hotel_Management_System;
+
+import java.util.*;
+
+class Singleroom 
+{
+    String name;
+    String contact;
+    String gender;   
+    ArrayList<Food> food =new ArrayList<>();
+
+   
+    Singleroom()
+    {
+        this.name="";
+    }
+    Singleroom(String name,String contact,String gender)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+    }
+}
+
+class Doubleroom extends Singleroom 
+{ 
+    String name2;
+    String contact2;
+    String gender2;  
+    
+    Doubleroom()
+    {
+        this.name="";
+        this.name2="";
+    }
+    Doubleroom(String name,String contact,String gender,String name2,String contact2,String gender2)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+        this.name2=name2;
+        this.contact2=contact2;
+        this.gender2=gender2;
+    }
+}
